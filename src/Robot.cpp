6bd1024fe3377c7d30d5d912951dee11fd3fa2c4
Me <asus.laptop@cybercards.com>
2015-01-31 11:46:15 -0500

    Cleaned Up TankDrive Code

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -3,16 +3,16 @@
 
 class Robot: public IterativeRobot
 {
-
 	RobotDrive myRobot; // robot drive system
-	Joystick stick; // only joystick
+	Joystick stick,stick2;
 	LiveWindow *lw;
 	int autoLoopCounter;
 
 public:
 	Robot() :
-		myRobot(0, 1),	// these must be initialized in the same order
-		stick(0),		// as they are declared above.
+		myRobot(0, 2, 1, 3),	// these must be initialized in the same order
+		stick(0),
+		stick2(1),				// as they are declared above.
 		lw(NULL),
 		autoLoopCounter(0)
 	{
@@ -52,17 +52,27 @@ private:
 
 	void TeleopPeriodic()
 	{
-		//myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
+		//Local declarations
+		float driveThreshold = 0.005;
 
 		//Get the x-axis of the joystick
-		float yAxisVal = stick.GetY();
-		std::cout << "yAxisVal: " << yAxisVal << std::endl;
+		float yAxis1 = stick.GetY();
+		float yAxis2 = stick2.GetY();
+
+		//std::cout << "yAxisVal: " << yAxisVal << std::endl;
 
 		//Convert input to [-1.0, 1.0] range
 		//Don't need to - already in the correct range
 
-		//Control the motor with PWM0 value
-		myRobot.Drive(yAxisVal, 0.0);
+		//Drive the drive motors when any input is within +-driveThreshold of 0.0
+		//NOTE - currently this doesn't scale up the input from 0.0 after the deadband region -- it just uses the raw value.
+		if(yAxis1 >= driveThreshold || yAxis2 >= driveThreshold || yAxis1 <= -driveThreshold || yAxis2 <= -driveThreshold )
+		{
+			myRobot.TankDrive(stick,stick2); 	// drive Forwards
+		} else {
+			myRobot.TankDrive(0.0, 0.0); 	// stop robot
+		}
+
 	}
 
 	void TestPeriodic()
